2751e098b7c5b4a527a5d5b10e8c2793d314b9c8
dianaefimova <diana.efimova@tuni.fi>
2022-11-08 19:47:53 +0200

    finalizing the page

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,32 +1,11 @@
-import React, { Component } from 'react';
-import {Route, Link, Routes, BrowserRouter, HashRouter } from "react-router-dom"
+import React from 'react';
+import {Route, Link, Routes, HashRouter } from "react-router-dom"
 import Home from "./Home";
 import About from "./About";
 import MyProjects from "./MyProjects";
 import './clouds.css';
 import './App.css';
-class App extends Component {
-    state = {
-        data: null
-      };
-    
-      componentDidMount() {
-        this.callBackendAPI()
-          .then(res => this.setState({ data: res.express }))
-          .catch(err => console.log(err));
-      }
-        // fetching the GET route from the Express server which matches the GET route from server.js
-      callBackendAPI = async () => {
-        const response = await fetch('/express_backend');
-        const body = await response.json();
-    
-        if (response.status !== 200) {
-          throw Error(body.message) 
-        }
-        return body;
-      };
-    
-      render() {
+function App() {
     return (
         <div className="cloudPane">
             <div className="stars">
@@ -208,5 +187,5 @@ class App extends Component {
         </div>
     )
 }
-      }
+      
 export default App;
